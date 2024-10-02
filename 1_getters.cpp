c7ae3d71c5d5b627b8df11dcc3349ec045aa5319
Humbert <122186863+xsabreusx@users.noreply.github.com>
2024-10-02 16:40:33 -0500

    Updated getters file

--- 1_getters.cpp
+++ 1_getters.cpp
@@ -25,7 +25,31 @@ public:
   {
     return this->width * this->height;
   }
+
+  void set_width(int w)
+  {
+    width = w;
+  }
+
+  void set_height(int h)
+  {
+    height = h;
+  }
+
 };
+
+Rectangle swap(Rectangle rectangle, int w, int  h)
+{
+  rectangle.set_height(h);
+  rectangle.set_width(w);
+  return rectangle;
+}
+
+void swapDirect(Rectangle &rectangle, int w, int  h)
+{
+  rectangle.set_height(h);
+  rectangle.set_width(w);
+}
 
 Rectangle scale(Rectangle rectangle, int factor)
 {
@@ -41,5 +65,16 @@ int main()
 
   Rectangle r2 = scale(r1, 2);
   cout << "Area of Rectangle 2: " << r2.area() << endl;
+
+  Rectangle r3 = swap(r2, 2, 2);
+  cout << "Area of New Rectangle 3 after swapping to sides of 2: " << r3.area() << endl;
+
+  swapDirect(r3, 3, 3);
+  cout << "Area of Rectangle 3 after direct swapping to sides of 3: " << r3.area() << endl;
+
+  cout << "Demonstration complete. Have a good day!" << endl;
+
   return 0;
+
+
 }
