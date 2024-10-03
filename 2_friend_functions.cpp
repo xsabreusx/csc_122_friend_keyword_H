95b15c6ee5cba359d9ce4f212e67710ebb473a7e
Humbert <122186863+xsabreusx@users.noreply.github.com>
2024-10-03 16:08:52 -0500

    completed part 1 and 2 with geometric swapper

--- 2_friend_functions.cpp
+++ 2_friend_functions.cpp
@@ -7,6 +7,7 @@ class Rectangle
 private:
   int width;
   int height;
+  friend class GeometricSwapper;
 
 public:
   Rectangle(int w, int h) : width(w), height(h) {}
@@ -17,6 +18,20 @@ public:
   }
 
   friend Rectangle scale(Rectangle, int);
+  
+};
+
+class GeometricSwapper
+{
+  private:
+  public:
+
+  void GeometricSwap(class Rectangle &rectangle, int h, int w )
+  {
+    rectangle.width = w;
+    rectangle.height = h;
+  }
+
 };
 
 Rectangle scale(Rectangle rectangle, int factor)
@@ -28,10 +43,16 @@ Rectangle scale(Rectangle rectangle, int factor)
 
 int main()
 {
+  GeometricSwapper swap;
+
   Rectangle r1(2, 3);
   cout << "Area of Rectangle 1: " << r1.area() << endl;
 
   Rectangle r2 = scale(r1, 2);
   cout << "Area of Rectangle 2: " << r2.area() << endl;
+
+  swap.GeometricSwap(r2,5,4);
+  cout << "Area of Rectangle 2 after GeometricSwap: " << r2.area() << endl;
+
   return 0;
 }
